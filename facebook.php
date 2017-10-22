d0c58cedd0bebbbbb66e97ed119e1df12e246420
rich88club <32992907+rich88club@users.noreply.github.com>
2017-10-22 12:24:55 +0700

    Update facebook.php

--- facebook.php
+++ facebook.php
@@ -4,18 +4,12 @@ $access_token = "EAAaQfgBMh4oBAFVAJYszAJBI9X2xxUQ7oik2gEskrZA9iMpmlA3I4QvmxqZAjZ
 $verify_token = "rich88";
 
 $hub_verify_token = null;
-error_log(isset($_REQUEST['hub_challenge']));
 if (isset($_REQUEST['hub_challenge'])) {
+    error_log('match request');
     $challenge = $_REQUEST['hub_challenge'];
     $hub_verify_token = $_REQUEST['hub_verify_token'];
 }
 echo $challenge;
-echo $hub_verify_token;
- error_log('challenge  : '. $challenge);
- error_log('hub_verify_token : '. $hub_verify_token);
 
-if ($hub_verify_token === $verify_token) {
-    echo $challenge;
-}
 
 ?>
